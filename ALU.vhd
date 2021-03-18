b080e6e822447d25004e5c7911926289eedac31b
Georgios Frangias <georgios.frangias@outlook.com>
2021-03-18 15:42:37 +0200

    Minor changes
    -Implemented other operations
    -Added comments

--- ALU.vhd
+++ ALU.vhd
@@ -32,22 +32,24 @@ entity ALU is
 	 
     --Outputs
     Output: out std_logic_vector(31 downto 0); --output result
+	 --Out33: out std_logic_vector(32 downto 0);
     Zero: out std_logic;                    --if output = 0
     Cout: out std_logic;                    --if there is carry out
-    Ovf: out std_logic);                     --if there is overflow
+    Ovf: out std_logic	                    --if there is overflow
+	 );
     
 end ALU;
 
 architecture Behavioral of ALU is
 
     --Temporary signals for output
-    signal tOutput: std_logic_vector(31 downto 0);
-    signal tOvf: std_logic;
-	 signal tmp33: std_logic_vector(32 downto 0);
+    signal tOutput: std_logic_vector(31 downto 0); --signal for the output
+    signal tOvf: std_logic;                         --signal to store Ovf variable
+	 signal tmp33: std_logic_vector(32 downto 0);   --signal for the output in 33 bits to calculate Cout
 begin
 
     process
-		variable OvfCheck: std_logic_vector(31 downto 0) := x"00000000";
+		variable OvfCheck: std_logic_vector(31 downto 0) := x"00000000";    --variable used to check if there is an Ovf
     begin
 			wait until clock' event and clock = '1';
 			
@@ -56,7 +58,7 @@ begin
 				OvfCheck := std_logic_vector(unsigned(a) + unsigned(b));
             tmp33 <= std_logic_vector(unsigned('0' & A) + unsigned('0' & B));
 				
-            if (A(31)=B(31) and OvfCheck(31)/=A(31)) then
+            if (A(31)=B(31) and OvfCheck(31)/=A(31)) then   --if A and B have the same sign and the result has the opposite
                 tOvf <= '1';
             else
                 tOvf <= '0';
@@ -67,7 +69,7 @@ begin
 				OvfCheck := std_logic_vector(unsigned(a) - unsigned(b));
             tmp33 <= std_logic_vector(unsigned('0' & A) - unsigned('0' & B));
 				
-            if (A(31)/=B(31) and OvfCheck(31)=B(31)) then
+            if (A(31)/=B(31) and OvfCheck(31)=B(31)) then   --if A and B have opposite signs and the result has the same as the subtrahend
                 tOvf <= '1';
             else
                 tOvf <= '0';
@@ -75,29 +77,48 @@ begin
 
         elsif Op = "0010" then
             tOutput <= A and B;   --logic AND
+				tOvf <= '0';
+				tmp33(32) <= '0';
         elsif Op = "0011" then
             tOutput <= A or B;   --logic OR
+            tOvf <= '0';
+				tmp33(32) <= '0';
         elsif Op = "0100" then
             tOutput <= not A;     --inversion (NOT)
+            tOvf <= '0';
+				tmp33(32) <= '0';
         elsif Op = "0110" then
             tOutput <= A nand B;  --logic NAND
+            tOvf <= '0';
+				tmp33(32) <= '0';
         elsif Op = "1000" then
             tOutput <= A(31) & A(31 downto 1);  --arithmetic shift right
+            tOvf <= '0';
+				tmp33(32) <= '0';
         elsif Op = "1001" then
             tOutput <= std_logic_vector(unsigned(A) srl 1);    --logic shift right
+            tOvf <= '0';
+				tmp33(32) <= '0';
         elsif Op = "1010" then
             tOutput <= std_logic_vector(unsigned(A) sll 1);    --logic shift left
+            tOvf <= '0';
+				tmp33(32) <= '0';
         elsif Op = "1100" then
             tOutput <= std_logic_vector(unsigned(A) rol 1);    --circular shift left
+            tOvf <= '0';
+				tmp33(32) <= '0';
         elsif Op = "1101" then
             tOutput <= std_logic_vector(unsigned(A) ror 1);	 --circular shift right
+            tOvf <= '0';
+				tmp33(32) <= '0';
         end if;
 	end process;
 	
 	Output <= tOutput after 10ns;
-	Zero <= '1' after 10ns when tOutput = x"00000000" else '0' after 10ns;
-	Cout <= tmp33(32) after 10ns;
+	Zero <= '1' after 10ns when tOutput = x"00000000" else '0' after 10ns; --if the result is 0 Zero='0'
+	Cout <= tmp33(32) after 10ns;   --check if there is carry out in the 33rd bit
 	Ovf <= tOvf after 10ns;
-	--Ovf <= '1' after 10ns when tOverflow = '1' and tOutput = else '0' after 10ns;
-end Behavioral;
+	--Out33 <= tmp33 after 10ns;
+	
+	end Behavioral;
 
