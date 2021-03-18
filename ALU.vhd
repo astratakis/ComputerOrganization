d5c46659cab733354b284d0cae8adff88f4c1c11
Georgios Frangias <georgios.frangias@outlook.com>
2021-03-18 11:22:10 +0200

    Implemented Ovf and Cout signals

--- ALU.vhd
+++ ALU.vhd
@@ -42,34 +42,35 @@ architecture Behavioral of ALU is
 
     --Temporary signals for output
     signal tOutput: std_logic_vector(31 downto 0);
-	 signal tOverflow: std_logic := '0';
-    signal tmp: std_logic_vector(32 downto 0);
-
+    signal tOvf: std_logic;
+	 signal tmp: std_logic_vector(32 downto 0);
 begin
 
     process
-    
+	 
     begin
 			wait until clock' event and clock = '1';
 			
         if Op = "0000" then
             tOutput <= A + B;   --addition
-				
-				if A(31) = B(31) then
-				tOverflow <= '1';
-				else
-				tOverflow <= '0';
-				end if; 
+            tmp <= std_logic_vector(unsigned('0' & A) + unsigned('0' & B));
+
+            if (A(31)=B(31) and tmp(31)/=A(31)) then
+                tOvf <= '1' after 10ns;
+            else
+                tOvf <= '0' after 10ns;
+            end if;
 				
         elsif Op = "0001" then
             tOutput <= A - B;   --subtraction
+            tmp <= std_logic_vector(unsigned('0' & A) - unsigned('0' & B));
 				
-				if A(31) /= B(31) then
-				tOverflow <= '1';
-				else
-				tOverflow <= '0';
-				end if; 
-				
+            if (A(31)/=B(31) and tmp(31)=B(31)) then
+                tOvf <= '1' after 10ns;
+            else
+                tOvf <= '0' after 10ns;
+            end if;
+
         elsif Op = "0010" then
             tOutput <= A and B;   --logic AND
         elsif Op = "0011" then
@@ -89,11 +90,12 @@ begin
         elsif Op = "1101" then
             tOutput <= std_logic_vector(unsigned(A) ror 1);	 --circular shift right
         end if;
-    
 	end process;
 	
 	Output <= tOutput after 10ns;
 	Zero <= '1' after 10ns when tOutput = x"00000000" else '0' after 10ns;
-	Ovf <= '1' after 10ns when tOverflow = '1' and tOutput = else '0' after 10ns;
+	Cout <= tmp(32) after 10ns;
+	Ovf <= tOvf after 10ns;
+	--Ovf <= '1' after 10ns when tOverflow = '1' and tOutput = else '0' after 10ns;
 end Behavioral;
 
