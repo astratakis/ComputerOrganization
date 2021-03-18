62ff1701dd9abd5bd7cbed1dfda46ed97e104eb1
Georgios Frangias <georgios.frangias@outlook.com>
2021-03-18 19:11:48 +0200

    Adding register

---
+++ register.vhd
@@ -0,0 +1,39 @@
+----------------------------------------------------------------------------------
+-- Institution:  Technical University of Crete
+-- Course:      Computer Architecture (HPY302)
+-- Engineer:    Georgios Frangias
+-- 
+-- Create Date:     16/03/2021 
+-- Module Name:     register - Behavioral 
+-- Project Name:    HPY302_LAB
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity register is
+end register;
+
+architecture Behavioral of register is
+
+begin
+
+
+end Behavioral;
+
